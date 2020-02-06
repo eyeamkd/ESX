b6069afd2d5632b2daff9f88aae0206dbf2261bf
kaustubh.e <kaustubh.e@DESKTOP-KE0CR33>
2020-02-06 13:13:38 +0530

    Added Stack Overflow Search

--- background.js
+++ background.js
@@ -1,12 +1,29 @@
 chrome.commands.onCommand.addListener(function(command) {
+    var bkg = chrome.extension.getBackgroundPage();
+    bkg.console.log('foo');
+    switch(command) {
+      case 'search-feature':
+        chrome.tabs.executeScript( {
+          code: "window.getSelection().toString();"
+        }, function(selection) {
+          // document.getElementById("output").value = selection[0];  
+          const newURL =  `https://www.google.com/search?q=${selection}`
+          chrome.tabs.create({ url: newURL });
+        });
+        return;
+      case 'stack-overflow-search':
+        chrome.tabs.executeScript( {
+          code: "window.getSelection().toString();"
+        }, function(selection) {
+          // document.getElementById("output").value = selection[0];  
+          const newURL =  `https://stackoverflow.com/search?q=${selection}`
+          chrome.tabs.create({ url: newURL });
+        });
+        return;
+
+    }
     console.log('Command:', command); 
     console.log("Selected word is ", window.getSelection().toString()); 
-    chrome.tabs.executeScript( {
-        code: "window.getSelection().toString();"
-      }, function(selection) {
-        // document.getElementById("output").value = selection[0];  
-        const newURL =  `https://www.google.com/search?q=${selection}`
-        chrome.tabs.create({ url: newURL });
-      });
+    
   });  
   console.log("Background Script Running");
